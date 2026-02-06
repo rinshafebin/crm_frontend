6a6f6b3e659724913666f71c8708b2c8284c6938
rinshafebin <rinshafebinkk12@gmail.com>
2026-02-06 10:21:42 +0530

    updated student page

--- LpCrm /src/Components/utils/studentConstants.js
+++ LpCrm /src/Components/utils/studentConstants.js
@@ -6,7 +6,7 @@ export const BATCH_CHOICES = [
   { value: 'A1 ONLINE', label: 'A1 (Online)' },
   { value: 'A2 ONLINE', label: 'A2 (Online)' },
   { value: 'B1 ONLINE', label: 'B1 (Online)' },
-  { value: 'ONLINE', label: 'Online)' },
+  { value: 'ONLINE', label: 'Online' },
   { value: 'A1 EXAM PREPERATION', label: 'A1 (Exam Preparation)' },
   { value: 'A2 EXAM PREPERATION', label: 'A2 (Exam Preparation)' },
   { value: 'B1 EXAM PREPERATION', label: 'B1 (Exam Preparation)' },
@@ -18,30 +18,6 @@ export const STATUS_CHOICES = [
   { value: 'PAUSED', label: 'Paused' },
   { value: 'COMPLETED', label: 'Completed' },
   { value: 'DROPPED', label: 'Dropped' },
-];
-
-export const CLASS_CHOICES = [
-  { value: 'C1', label: 'C1' },
-  { value: 'C2', label: 'C2' },
-  { value: 'C3', label: 'C3' },
-  { value: 'C4', label: 'C4' },
-  { value: 'C5', label: 'C5' },
-  { value: 'C6', label: 'C6' },
-  { value: 'C7', label: 'C7' },
-  { value: 'C8', label: 'C8' },
-  { value: 'C9', label: 'C9' },
-  { value: 'C10', label: 'C10' },
-  { value: 'C11', label: 'C11' },
-  { value: 'C12', label: 'C12' },
-  { value: 'C13', label: 'C13' },
-  { value: 'C14', label: 'C14' },
-  { value: 'C15', label: 'C15' },
-  { value: 'C16', label: 'C16' },
-  { value: 'C17', label: 'C17' },
-  { value: 'C18', label: 'C18' },
-  { value: 'C19', label: 'C19' },
-  { value: 'C20', label: 'C20' },
-  { value: 'ONLINE', label: 'Online' },
 ];
 
 export const initialStudentFormData = {
