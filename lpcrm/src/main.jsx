a5ac14d35bcfe6ab98ee0a4882181aa34f3a21a5
Rinsha Febin <rinshafebinkk12@gmail.com>
2026-04-07 10:12:11 +0530

    Update main.jsx

--- lpcrm/src/main.jsx
+++ lpcrm/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
+import { PusherProvider } from './context/PusherContext';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+        <PusherProvider>
+          <App />
+        </PusherProvider>
     </AuthProvider>
   </StrictMode>,
 )
