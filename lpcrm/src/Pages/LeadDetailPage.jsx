426e57b3626dbce2b2a7afae220ac377b3229bf5
Rinsha Febin <rinshafebinkk12@gmail.com>
2026-04-13 10:47:25 +0530

    Update LeadDetailPage.jsx

--- lpcrm/src/Pages/LeadDetailPage.jsx
+++ lpcrm/src/Pages/LeadDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { 
   ArrowLeft, User, Phone, Mail, MapPin, Calendar, Tag, 
